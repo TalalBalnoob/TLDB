c966a101cf5c758c2a091cc4e5da5f762d094b6f
Talal Balnoob <TalalBalnoob@gmail.com>
2026-03-01 23:18:51 +0300

    make the base REPL

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program {
+	static void Main(string[] args) {
+		while (true) {
+			PrintPrompt();
+
+			string? input = Console.ReadLine().Trim();
+
+			if (string.IsNullOrWhiteSpace(input))
+				continue;
+
+			if (input == ".exit") {
+				break;
+			}
+			else {
+				Console.WriteLine($"Unrecognized command '{input}'.");
+			}
+		}
+	}
+
+	static void PrintPrompt() {
+		Console.Write("db > ");
+	}
+}
